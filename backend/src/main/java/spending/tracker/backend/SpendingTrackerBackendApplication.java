ba48808842b44319c9a67d9c86c3275d89c71b62
TerpsihoraNSK <arsenjeva.ea@gmail.com>
2026-04-28 23:56:20 +0700

    fix design

--- backend/src/main/java/spending/tracker/backend/SpendingTrackerBackendApplication.java
+++ backend/src/main/java/spending/tracker/backend/SpendingTrackerBackendApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpendingTrackerBackendApplication {
 
     static void main(String[] args) {
-        // Load environment variables from .env file
         Dotenv dotenv = Dotenv.load();
         dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 
